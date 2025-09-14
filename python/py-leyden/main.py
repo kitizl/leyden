2176566365ee97593bbe7bb2eef499615eb5a544
kitizl <nithesh.dragoon98@gmail.com>
2025-09-14 19:09:54 +0200

    Added some functions that I thought up and stole from the Game Design Patterns guy

--- python/py-leyden/main.py
+++ python/py-leyden/main.py
@@ -1,3 +1,18 @@
+def markdown_to_html(markdown_text : str) -> str:
+    html_text : str = ""
+    return html_text
+
+def html_prettify(text : str) -> str:
+    '''Use HTML codes for certain characters.'''
+    html_code_dict = {
+        " -- " : "&#8202;&mdash;&#8202;",
+        "à": "&agrave;",
+        "ï": "&iuml;",
+        "ø": "&oslash;",
+        "æ": "&aelig;"
+    }
+    return text.translate(str.maketrans(html_code_dict))
+
 def main():
     print("Hello from py-leyden!")
 
